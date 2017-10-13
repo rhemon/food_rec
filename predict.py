cb1b48b3affd858db72a012af6c047aad730c98c
Ridhwanul Haque <rhemon19@gmail.com>
2017-10-13 15:22:44 +0600

    [12, 4], 0.01 learning rate, 10000 iterations

--- predict.py
+++ predict.py
@@ -33,38 +33,38 @@ print("Accuracy with training set,",str(((np.sum(yHat == Y))/(Y.shape[0]*X.shape
 yTest = predict(dev_set, params)
 print("Accuracy with dev set,",str(((np.sum(yTest == dev_set_y))/(dev_set_y.shape[0]*dev_set.shape[1])*100)) + "%")
 
-# check out 500 mangos how many it can recognise
-mangoExamples = np.zeros((4800, 450))
-mangoExamples[:, 0:400] = X[:,0:400]
-mangoExamples[:, 400:450] = dev_set[:, 0:50]
-mangoPredict = predict(mangoExamples, params)
-expectedOutput_mango = np.zeros((4, mangoExamples.shape[1]))
-expectedOutput_mango[0,:] = 1
-print("Accuracy in recognising mango:", (np.sum(mangoPredict == expectedOutput_mango)/(4*450)) * 100)
+# # check out 500 mangos how many it can recognise
+# mangoExamples = np.zeros((4800, 450))
+# mangoExamples[:, 0:400] = X[:,0:400]
+# mangoExamples[:, 400:450] = dev_set[:, 0:50]
+# mangoPredict = predict(mangoExamples, params)
+# expectedOutput_mango = np.zeros((4, mangoExamples.shape[1]))
+# expectedOutput_mango[0,:] = 1
+# print("Accuracy in recognising mango:", (np.sum(mangoPredict == expectedOutput_mango)/(4*450)) * 100)
 
-# check out 500 apples how many it can recognise
-appleExamples = np.zeros((4800,450))
-appleExamples[:, 0:400] = X[:,400:800]
-appleExamples[:, 400:450] = dev_set[:,50:100]
-applePredict = predict(appleExamples, params)
-expectedOutput_apple = np.zeros((4, appleExamples.shape[1]))
-expectedOutput_apple[1,:] = 1
-print("Accuracy in recognising apple:", (np.sum(applePredict == expectedOutput_apple)/(4*450)) * 100)
+# # check out 500 apples how many it can recognise
+# appleExamples = np.zeros((4800,450))
+# appleExamples[:, 0:400] = X[:,400:800]
+# appleExamples[:, 400:450] = dev_set[:,50:100]
+# applePredict = predict(appleExamples, params)
+# expectedOutput_apple = np.zeros((4, appleExamples.shape[1]))
+# expectedOutput_apple[1,:] = 1
+# print("Accuracy in recognising apple:", (np.sum(applePredict == expectedOutput_apple)/(4*450)) * 100)
 
-# check out 500 apples how many it can recognise
-orangeExamples = np.zeros((4800,450))
-orangeExamples[:, 0:400] = X[:,800:1200]
-orangeExamples[:, 400:450] = dev_set[:, 100:150]
-orangePredict = predict(orangeExamples, params)
-expectedOutput_orange = np.zeros((4, orangeExamples.shape[1]))
-expectedOutput_orange[2,:] = 1
-print("Accuracy in recognising orange:", (np.sum(orangePredict == expectedOutput_orange)/(4*450)) * 100)
+# # check out 500 apples how many it can recognise
+# orangeExamples = np.zeros((4800,450))
+# orangeExamples[:, 0:400] = X[:,800:1200]
+# orangeExamples[:, 400:450] = dev_set[:, 100:150]
+# orangePredict = predict(orangeExamples, params)
+# expectedOutput_orange = np.zeros((4, orangeExamples.shape[1]))
+# expectedOutput_orange[2,:] = 1
+# print("Accuracy in recognising orange:", (np.sum(orangePredict == expectedOutput_orange)/(4*450)) * 100)
 
-# check out 500 apples how many it can recognise
-pearExamples = np.zeros((4800,450))
-pearExamples[:, 0:400] = X[:,1200:1600]
-pearExamples[:, 400:450] = dev_set[:,150:]
-pearPredict = predict(pearExamples, params)
-expectedOutput_pear = np.zeros((4, pearExamples.shape[1]))
-expectedOutput_pear[3,:] = 1
-print("Accuracy in recognising pear:", (np.sum(pearPredict == expectedOutput_pear)/(4*450)) * 100)
+# # check out 500 apples how many it can recognise
+# pearExamples = np.zeros((4800,450))
+# pearExamples[:, 0:400] = X[:,1200:1600]
+# pearExamples[:, 400:450] = dev_set[:,150:]
+# pearPredict = predict(pearExamples, params)
+# expectedOutput_pear = np.zeros((4, pearExamples.shape[1]))
+# expectedOutput_pear[3,:] = 1
+# print("Accuracy in recognising pear:", (np.sum(pearPredict == expectedOutput_pear)/(4*450)) * 100)
