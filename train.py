e437621d4057626fd60d4ab6054697ec7f0f0a0b
Ridhwanul Haque <rhemon19@gmail.com>
2017-10-18 14:41:49 +0600

    Organizing train.py
    breaking down parts of training model into helper functions

--- train.py
+++ train.py
@@ -24,9 +24,42 @@ def init_params(layers):
 
     return params
 
-def model(X, Y, params, layers, learning_rate, iteration, print_cost, iter_count):
+def forward_prop(X, params, nl):
+    cache = {"A0": X}
+    for j in range(nl-2):
+        cache["Z"+str(j+1)] = np.array(np.dot(params["W"+str(j+1)], cache["A"+str(j)]) + params["b"+str(j+1)])
+        cache["A"+str(j+1)] = sigmoid(cache["Z"+str(j+1)])
+    cache["Z"+str(nl-1)] = np.dot(params["W"+str(nl-1)], cache["A"+str(nl-2)]) + params["b"+str(nl-1)]
+    cache["T"] = np.array(np.exp(cache["Z"+str(nl-1)]))
+    cache["A"+str(nl-1)] = (cache["T"]/np.sum(cache["T"], axis=0, keepdims=True))
+    return cache
+
+def compute_cost(cache, Y, m, nl):
+    logprobs = -1 * np.sum(np.multiply(np.log(cache["A"+str(nl-1)]), Y), axis=0, keepdims=True)
+    cost = np.squeeze((1/(m)) * np.sum(logprobs, axis=1, keepdims=True))
+
+    return cost
+
+def back_prop(cache, params, Y, m, nl):
+    grads = {}
+    grads["dZ"+str(nl-1)] = cache["A"+str(nl-1)] - Y
+    for k in reversed(range(nl-1)):
+        grads["dW"+str(k+1)] = 1/(m) * np.dot(grads["dZ"+str(k+1)], cache["A"+str(k)].T)
+        grads["db"+str(k+1)] = 1/(m) * np.sum(grads["dZ"+str(k+1)], axis=1, keepdims=True, dtype='float')
+        grads["dZ"+str(k)] = np.dot(params["W"+str(k+1)].T, grads["dZ"+str(k+1)]) * (cache["A"+str(k)] * (1 - cache["A"+str(k)]))
+
+    return grads
+
+def gradient_descent(params, grads, learning_rate, nl):
+    for l in range(nl-1):
+        params["W"+str(l+1)] -= (grads["dW"+str(l+1)] * learning_rate)
+        params["b"+str(l+1)] -= (grads["db"+str(l+1)] * learning_rate)
+
+    return params
+
+def model(oX, oY, params, layers, learning_rate, iteration, print_cost, iter_count):
     # Useful variables
-    m = X.shape[1]
+    m = oX.shape[1]
 #     n_x = X.shape[0]
 #     n_h = X.shape[0]
 #     n_y = 4
@@ -34,34 +67,32 @@ def model(X, Y, params, layers, learning_rate, iteration, print_cost, iter_count
     iterations = []
     nl = len(layers)
     for i in range(iteration):
+
+        # Shuffle Training set
+        TrainingSet = np.append(oX, oY, axis=0)
+        np.random.shuffle(np.transpose(TrainingSet))
+        X = TrainingSet[:4800, :]
+        Y = TrainingSet[4800:, :]
+
         # Forward Propagation
-        cache = {"A0": X}
-        for j in range(nl-1):
-            cache["Z"+str(j+1)] = np.dot(params["W"+str(j+1)], cache["A"+str(j)]) + params["b"+str(j+1)]
-            cache["A"+str(j+1)] = sigmoid(cache["Z"+str(j+1)])
-        
-        logprobs = np.multiply(np.log(cache["A"+str(nl-1)]), Y) + np.multiply(np.log(1-cache["A"+str(nl-1)]), 1-Y)
-        cost = np.squeeze((-1/(m*layers[-1])) * np.sum(logprobs))
+        cache = forward_prop(X, params, nl)
+
+        cost = compute_cost(cache, Y, m, nl)
 
         # Backward Propagation
-        grads = {}
-        grads["dZ"+str(nl-1)] = cache["A"+str(nl-1)] - Y
-        for k in reversed(range(nl-1)):
-            grads["dW"+str(k+1)] = 1/(m) * np.dot(grads["dZ"+str(k+1)], cache["A"+str(k)].T)
-            grads["db"+str(k+1)] = 1/(m) * np.sum(grads["dZ"+str(k+1)], axis=1, keepdims=True, dtype='float')
-            grads["dZ"+str(k)] = np.dot(params["W"+str(k+1)].T, grads["dZ"+str(k+1)]) * (cache["A"+str(k)] * (1 - cache["A"+str(k)]))
+        grads = back_prop(cache, params, Y, m, nl)
 
         # Gradient descent
-        for l in range(nl-1):
-            params["W"+str(l+1)] -= (grads["dW"+str(l+1)] * learning_rate)
-            params["b"+str(l+1)] -= (grads["db"+str(l+1)] * learning_rate)
+        params = gradient_descent(params, grads, learning_rate, nl)
         
-        if ((i+1) % 20 == 0) and print_cost:
-            print("Cost after",(iter_count*100)+(i+1),"turn:",cost)
+        # Print cost
+        if ((i+1) % 100 == 0) and print_cost:
+            print("Cost after",(iter_count*iteration)+(i+1),"turn:",cost)
         if i > 1:
             if cost > costs[i-1]:
-                print("cost increased at", str((iter_count*100)+i)+"th turn")
+                print("cost increased at", str((iter_count*iteration)+i)+"th turn")
         costs.append(cost)
         iterations.append(i+1)
+
     return params, costs, iterations
 
