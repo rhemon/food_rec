253d800d7621aa2756f368df61f7000a122506f1
Ridhwanul Haque <rhemon19@gmail.com>
2017-10-21 12:30:02 +0600

    [24, 12, 4], 0.01000 learning rate, 40000 iterations

--- init_params.py
+++ init_params.py
@@ -2,5 +2,5 @@ import numpy as np
 import train
 
 X = np.load("data/X.npy")
-params = train.init_params([X.shape[0], 12, 4])
+params = train.init_params([X.shape[0], 24, 12, 4])
 np.save("params.npy", params)
