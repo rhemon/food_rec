88c16d0d751b94d3fb0dc4a84168139ff39fa22e
Ridhwanul Haque <rhemon19@gmail.com>
2017-10-21 12:33:49 +0600

    [24, 12, 4], 0.03000 learning rate, 40000 iterations

--- ftrain_nn.py
+++ ftrain_nn.py
@@ -6,5 +6,5 @@ X = np.load("data/X.npy")
 Y = np.load("data/Y.npy")
 
 params = np.load("params.npy").item()
-params, costs, iters = train.model(X, Y, params, [X.shape[0], 24, 12, 4], 0.02500, 4000, True, int(sys.argv[1]))
+params, costs, iters = train.model(X, Y, params, [X.shape[0], 24, 12, 4], 0.03000, 4000, True, int(sys.argv[1]))
 np.save("params.npy", params)
